1c08524a0e9d2ee0fe12c1ccbce352554b14c01b
Thedywender <thedywender@hotmail.com>
2024-04-16 16:54:14 -0400

    req02, req03, req04 ok

--- src/models/language_model.py
+++ src/models/language_model.py
@@ -1,9 +1,18 @@
+from typing import Dict, List
 from database.db import db
-from abstract_model import AbstractModel
+from models.abstract_model import AbstractModel
 
 
 class LanguageModel(AbstractModel):
     _collection = db["languages"]
 
-    def __init__(self, data: dict):
+    def __init__(self, data: Dict[str, str]):
         super().__init__(data)
+
+    def to_dict(self) -> Dict[str, str]:
+        return {"name": self.data["name"], "acronym": self.data["acronym"]}
+
+    @classmethod
+    def list_dicts(cls) -> List[Dict[str, str]]:
+        languages = cls._collection.find()
+        return [cls(language).to_dict() for language in languages]
