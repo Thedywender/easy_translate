c1dfd4252f10d25e1f2772a4b55e45c42a76397e
Thedywender <thedywender@hotmail.com>
2024-04-16 15:51:47 -0400

    initial commit

---
+++ src/database/db.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+import os
+
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+db_client = MongoClient(mongo_uri)
+db = db_client[os.getenv("DB_NAME", "test_db_traduzo")]
