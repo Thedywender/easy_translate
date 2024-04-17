1a397cba7e835730ff12ae62dcd40317b76a7cff
Thedywender <thedywender@hotmail.com>
2024-04-16 20:27:26 -0400

    req06 ok

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, request
 from src.models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 language = Blueprint("language", __name__)
 
@@ -8,14 +8,35 @@ language = Blueprint("language", __name__)
 @language.get("/")
 def render_language_page():
     languages = LanguageModel.list_dicts()
-    text_translate = "O que deseja traduzir?"
+    text_to_translate = "O que deseja traduzir?"
     translate_from = "pt"
     translate_to = "en"
     translated = "What do you want to translate?"
     return render_template(
         "index.html",
         languages=languages,
-        text_to_translate=text_translate,
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated,
+    )
+
+
+@language.post("/")
+def translate_query():
+    languages = LanguageModel.list_dicts()
+    text_to_translate = request.form["text-to-translate"]
+    translate_from = request.form["translate-from"]
+    translate_to = request.form["translate-to"]
+
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=text_to_translate,
         translate_from=translate_from,
         translate_to=translate_to,
         translated=translated,
