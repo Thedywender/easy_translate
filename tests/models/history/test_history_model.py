ec01ab24e75f340b6538f98ca4442cd124a4b79f
Thedywender <thedywender@hotmail.com>
2024-04-18 13:02:14 -0400

    req08 ok

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,27 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history_json = HistoryModel.list_as_json()
+    history_data = json.loads(history_json)
+
+    base_data = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+
+    for data in history_data:
+        del data["_id"]
+
+    for data in base_data:
+        assert data in history_data
