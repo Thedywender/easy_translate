d707b9664f56b8256e70dec33b03a8b833756955
Thedywender <thedywender@hotmail.com>
2024-05-28 17:45:52 -0400

    finaly

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -10,17 +10,17 @@ language = Blueprint("language", __name__)
 @language.get("/")
 def render_language_page():
     languages = LanguageModel.list_dicts()
-    text_to_translate = "O que deseja traduzir?"
+    # text_to_translate = "O que deseja traduzir?"
     translate_from = "pt"
     translate_to = "en"
-    translated = "What do you want to translate?"
+    # translated = "What do you want to translate?"
     return render_template(
         "index.html",
         languages=languages,
-        text_to_translate=text_to_translate,
+        # text_to_translate=text_to_translate,
         translate_from=translate_from,
         translate_to=translate_to,
-        translated=translated,
+        # translated=translated,
     )
 
 
